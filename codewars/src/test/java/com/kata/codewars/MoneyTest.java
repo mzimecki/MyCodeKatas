286fbe0e2ced46a23cecc5a9e0d48d0ac3202bab
Marcin Zimecki <marcin.zimecki@gmail.com>
2017-02-05 18:27:46 +0100

    refactored

--- codewars/src/test/java/com/kata/codewars/MoneyTest.java
+++ codewars/src/test/java/com/kata/codewars/MoneyTest.java
@@ -11,5 +11,6 @@ public class MoneyTest {
         assertEquals(3, Money.calculateYears(1000,0.05,0.18,1100));
         assertEquals(14 , Money.calculateYears(1000,0.01625,0.18,1200));
         assertEquals(0, Money.calculateYears(1000,0.05,0.18,1000));
+        assertEquals(0, Money.calculateYears(2000,0.05,0.18,1000));
     }
 }
